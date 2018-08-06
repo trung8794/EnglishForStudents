1f1c07ce46e90b069dff9949e372f0826ea8e930
Nguyen Van Trung <johnvinhuni@gmail.com>
2018-08-06 17:49:20 +0700

    174906082018Change

--- EnglishForStudents/Controller/NextLoginViewController.swift
+++ EnglishForStudents/Controller/NextLoginViewController.swift
@@ -31,13 +31,16 @@ class NextLoginViewController: UIViewController, UITableViewDataSource, UITableV
                                  "Some detail of Grade 7",
                                  ]
     
-    //------------------------
+    //----
     var TableViewInside: UITableView = UITableView()
     var SlideView : UIView = UIView()
     var Mark : UIImageView! =  UIImageView()
     var ButtonLeft : UIButton!
-    
-   
+    //----
+    var lastContentOffset: CGFloat = 0
+    var heightScrollView : CGFloat = 0
+    var widthScrollView : CGFloat = 0
+    var changeHeight = 0.4
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
@@ -59,60 +62,108 @@ class NextLoginViewController: UIViewController, UITableViewDataSource, UITableV
         mySliderFeature.contentSize = CGSize(width: mySliderFeature.bounds.width * CGFloat(pageCount), height: mySliderFeature.bounds.height)
         
         for index in 0...pageCount - 1{
-            if index == 0{
-                myFrame.origin.y = 0
-                myFrame.origin.x = mySliderFeature.frame.size.width * CGFloat(index) + 50
-                myFrame.size.width = mySliderFeature.frame.size.width - 100
-                myFrame.size.height = mySliderFeature.frame.size.height
-            }
-            else{
-                myFrame.origin.y = 20
-                myFrame.origin.x = mySliderFeature.frame.size.width * CGFloat(index) - 30
-                myFrame.size.width = mySliderFeature.frame.size.width + 60
-                myFrame.size.height = mySliderFeature.frame.size.height - 40
-            }
+
+            var parentView : UIView = UIView(frame: CGRect(x: mySliderFeature.frame.width * CGFloat(index), y: 0, width: mySliderFeature.frame.width, height: mySliderFeature.frame.height))
+            
+            self.mySliderFeature.addSubview(parentView)
             
             
-            var indexView :UIView = UIView(frame: myFrame)
-            indexView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            
-            var indexLabel = UILabel()
-            indexLabel.text = String(index)
-            indexLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-            indexLabel.font = UIFont(name: "Avenir Next Condensed", size: 80)
+            var indexView: UIView = UIView()
+  
+         
+            indexView.frame.size.height =  parentView.frame.height - 30
+            indexView.frame.size.width =  parentView.frame.width - 50
+            indexView.center.y = parentView.center.y
+            indexView.center.x = parentView.bounds.width / 2
+            indexView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            indexView.alpha = 0.8
             indexView.customBorderRadius()
+            parentView.addSubview(indexView)
             
-            indexView.addSubview(indexLabel)
-            self.mySliderFeature.addSubview(indexView)
+            var indexSubView : UIView = UIView()
+            indexSubView.frame.size.height =  150
+            indexSubView.frame.size.width =  200
+            indexSubView.center.y = indexView.bounds.height / 2
+            indexSubView.center.x = indexView.bounds.width / 2
+            indexSubView.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+            indexView.addSubview(indexSubView)
+            
+          
+          
+            var labelViewNumber: UILabel = UILabel()
+            labelViewNumber.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+            labelViewNumber.center = CGPoint(x: indexSubView.frame.width / 2, y: indexSubView.frame.height / 2)
+            labelViewNumber.text = String(index)
+            labelViewNumber.font = UIFont(name: "Avenir Next Condensed", size: 100)
+            labelViewNumber.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            labelViewNumber.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+            labelViewNumber.numberOfLines = 0
+            labelViewNumber.textAlignment = .center
+            indexSubView.addSubview(labelViewNumber)
             
             
-            let center_top = NSLayoutConstraint(item: indexLabel, attribute: .centerY, relatedBy: .equal, toItem: indexView, attribute: .centerY, multiplier: 1, constant: 0)
-            let center_left = NSLayoutConstraint(item: indexLabel, attribute: .centerX, relatedBy: .equal, toItem: indexView, attribute: .centerX, multiplier: 1, constant: 0)
-//            let width = NSLayoutConstraint(item: indexLabel, attribute: .width, relatedBy: .equal, toItem: indexView, attribute: .width, multiplier: 1, constant: 0)
-//            let height = NSLayoutConstraint(item: indexLabel, attribute: .height, relatedBy: .equal, toItem: indexView, attribute: .height, multiplier: 1/2, constant: 0)
-            indexView.autoresizesSubviews = false
-            indexLabel.translatesAutoresizingMaskIntoConstraints = false
-            view.addConstraints([center_top, center_left])
 
-
-            
+            //-- action Tap ---
+            let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+            indexView.addGestureRecognizer(tap)
+            indexView.isUserInteractionEnabled = true
             
         }
-        
         
         mySliderFeature.delegate = self
         mySliderFeature.isPagingEnabled = true
         mySliderFeature.showsVerticalScrollIndicator = false
         mySliderFeature.showsHorizontalScrollIndicator = false
-        print("Make done Scroll View")
+        
+        heightScrollView = mySliderFeature.frame.height
+        widthScrollView = mySliderFeature.frame.width
     }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        print("Hello World")
+    }
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
         var index = Int(scrollView.contentOffset.x / self.mySliderFeature.frame.width)
         mySliderTab.currentPage = index
-        let contentX = scrollView.contentOffset.x
-        let widthX = self.mySliderFeature.frame.width
-        var soDu = contentX / widthX
-        print("\(soDu)")
+        
+//        let contentX = scrollView.contentOffset.x
+//        let widthX = self.mySliderFeature.frame.width
+//        let tile = contentX / widthX - CGFloat(index)
+//        print("Index la : \(index) , contentx/width \(contentX / widthX), title\(tile) , 1- title \(1-tile)")
+//
+//        //-- change size when scroll--
+//        if (lastContentOffset > scrollView.contentOffset.x) {
+//            if(index > 0){
+//
+//                self.view.viewWithTag(100 + index)?.frame.size.height = heightScrollView + 0.004
+//                heightScrollView = (self.view.viewWithTag(100 + index)?.frame.height)!
+////                self.view.viewWithTag(100 + index)?.frame.size.width = (self.view.viewWithTag(100 + index)?.frame.width)! + (100.0 * tile)
+//                print(1 - tile)
+//                print("inside : \((self.view.viewWithTag(100 + index)?.frame.height)! - (40 * tile))")
+//            }
+//        }
+//        else if (lastContentOffset < scrollView.contentOffset.x) {
+//            // move down
+//        }
+//        lastContentOffset = scrollView.contentOffset.x
+//
+//
+//
+//        //-- last state of View Index ----
+//        UIView.animate(withDuration: 0.2) {
+//            self.view.viewWithTag(100 + index)?.alpha = 1
+//            if contentX / widthX == CGFloat(index) {
+//                 self.view.viewWithTag(100 + index)?.frame = CGRect(x: CGFloat(index) * self.mySliderFeature.frame.size.width + 50 , y: 0, width: self.mySliderFeature.frame.size.width - 100, height:self.mySliderFeature.frame.size.height)
+//            }
+//            for i in 0...self.pageCount{
+//                if(i != index && contentX / widthX == CGFloat(index)){
+//                    self.view.viewWithTag(100 + i)?.frame = CGRect(x: CGFloat(i) * self.mySliderFeature.frame.size.width - 30 , y: 20, width: self.mySliderFeature.frame.size.width + 60, height:self.mySliderFeature.frame.size.height - 40)
+//                    self.view.viewWithTag(100 + i)?.alpha = 0.5
+//                }
+//            }
+//        }
         txtShowDetail.text = myTextList[index] + "\n - Bla bla bla " + "\n - Bla bla bla " + "\n - Bla bla bla "
     }
     //------------------------
